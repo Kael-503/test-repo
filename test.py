7d7500e834e996b6e6f86c5772022c193c15d47d
zLPaslo97 <lilyanabelle2011@gmail.com>
2026-04-24 12:30:26 -0400

    new branch commit

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 print("kael")
-print("Lily")
+print("lily.")
+
+with open("nameFile.txt", "a") as file:
+    file.write("I have made this branch to test branches")
+print("a new branche has been made")
